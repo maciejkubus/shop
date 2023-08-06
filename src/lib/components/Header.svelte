97be3eabd10d43a33a0c0fed2dcb000f2546a683
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-06 23:59:28 +0200

    + header logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,12 +2,12 @@
 </script>
 
 <header class="absolute top-0 left-0 right-0 w-full bg-primary-500 shadow-md overflow-hidden">
-	<div class="max-w-site mx-auto flex gap-8 h-12 items-center">
-		<div class="w-1/4">
-			<div class="py-2">
+	<div class="max-w-site mx-auto flex gap-8 h-16 items-center">
+		<div class="w-1/4 flex gap-4 items-center text-white">
+			<div class="flex h-16 py-2">
 				<slot name="image" />
 			</div>
-			<h1>
+			<h1 class="font-black text-3xl tracking-wide">
 				<slot name="title" />
 			</h1>
 		</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,22 @@
 	import '../style/app.postcss';
 	import { AppShell } from '@skeletonlabs/skeleton';
 	import Header from '$lib/components/Header.svelte';
+	import { config } from '$lib/config';
+	import Logo from '$lib/components/Logo.svelte';
 </script>
 
 <AppShell>
 	<svelte:fragment slot="header">
-		<Header />
+		<Header>
+			<svelte:fragment slot="image">
+				<a class="contents" href="/">
+					<Logo />
+				</a>
+			</svelte:fragment>
+			<svelte:fragment slot="title">
+				<a class="text-white before:bg-white" href="/"> {config.siteName} </a>
+			</svelte:fragment>
+		</Header>
 	</svelte:fragment>
 	<slot />
 </AppShell>
