1039091c0863f488ccd75a2c183db9e7bfa1cf41
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-07 00:28:20 +0200

    + header menu

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 </script>
 
-<header class="absolute top-0 left-0 right-0 w-full bg-primary-500 shadow-md overflow-hidden">
+<header
+	class="absolute top-0 left-0 right-0 w-full variant-filled-primary shadow-md overflow-hidden"
+>
 	<div class="max-w-site mx-auto flex gap-8 h-16 items-center">
-		<div class="w-1/4 flex gap-4 items-center text-white">
+		<div class="w-1/4 flex gap-4 items-center">
 			<div class="flex h-16 py-2">
 				<slot name="image" />
 			</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,14 +11,21 @@
 <AppShell>
 	<svelte:fragment slot="header">
 		<Header>
-			<svelte:fragment slot="image">
-				<a class="contents" href="/">
-					<Logo />
-				</a>
-			</svelte:fragment>
-			<svelte:fragment slot="title">
-				<a class="text-white before:bg-white" href="/"> {config.siteName} </a>
-			</svelte:fragment>
+			<a slot="image" class="contents" href="/">
+				<Logo />
+			</a>
+			<a slot="title" class="text-white before:bg-white" href="/">
+				{config.siteName}
+			</a>
+			<nav slot="menu" class="w-full flex justify-center items-center">
+				<ul class="flex gap-4 text-xl">
+					<li><a href="/" class="text-white before:bg-white">Home</a></li>
+					<li><a href="/" class="text-white before:bg-white">Products</a></li>
+					<li><a href="/" class="text-white before:bg-white whitespace-nowrap">Buy again</a></li>
+					<li><a href="/" class="text-white before:bg-white whitespace-nowrap">Gift cards</a></li>
+					<li><a href="/" class="text-white before:bg-white">Contact</a></li>
+				</ul>
+			</nav>
 		</Header>
 	</svelte:fragment>
 	<slot />
