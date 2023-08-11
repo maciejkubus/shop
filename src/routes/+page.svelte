882321870f31dbf1013e5129852715fc09012778
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-12 00:39:17 +0200

    + 2nd product card

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,7 @@
 		<div class="w-1/3">
 			<ProductCard card="aspect-square bg-transparent" href="#">
 				<img slot="background" src="/images/headphones.jpg" alt="headphones" />
-				<div slot="header" class="text-xl font-bold drop-shadow-xl">Today's deal</div>
+				<div slot="header" class="text-2xl font-bold drop-shadow-xl">Today's deal</div>
 				<div class="w-full h-full flex justify-end items-end text-8xl font-bold drop-shadow-xl">
 					$39
 				</div>
@@ -16,6 +16,22 @@
 				</div>
 			</ProductCard>
 		</div>
-		<div class="w-2/3 bg-secondary">2</div>
+		<div class="w-2/3 bg-secondary">
+			<ProductCard card="bg-transparent" href="#">
+				<img
+					slot="background"
+					src="/images/laptops.jpg"
+					alt="headphones"
+					class="w-full h-full object-cover"
+				/>
+				<div slot="header" class="text-2xl font-bold drop-shadow-xl">Summer sale</div>
+				<div class="w-full h-full flex justify-end items-end text-8xl font-bold drop-shadow-xl">
+					$1999
+				</div>
+				<div slot="footer" class="text-right text-4xl drop-shadow-xl font-heading-token">
+					Laptops
+				</div>
+			</ProductCard>
+		</div>
 	</div>
 </div>
