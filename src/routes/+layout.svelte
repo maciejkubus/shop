35fa60d7aa7a3775ff64e02451f61d3ed55f40a8
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-11 23:53:47 +0200

    change dir stucture, + swiper

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,9 @@
 	import '../style/app.postcss';
 	import '@fortawesome/fontawesome-free/js/all.js';
 	import { AppShell } from '@skeletonlabs/skeleton';
-	import Header from '$lib/components/Header.svelte';
 	import { config } from '$lib/config';
-	import Logo from '$lib/components/Logo.svelte';
+	import Header from '$lib/components/header/Header.svelte';
+	import Logo from '$lib/components/logo/Logo.svelte';
 </script>
 
 <AppShell regionPage="pt-16">
@@ -23,31 +23,33 @@
 					<li><a href="/" class="text-tertiary-500 before:bg-tertiary-500">Home</a></li>
 					<li><a href="/" class="text-tertiary-500 before:bg-tertiary-500">Products</a></li>
 					<li>
-						<a href="/" class="text-tertiary-500 before:bg-tertiary-500 whitespace-nowrap"
-							>Buy again</a
-						>
+						<a href="/" class="text-tertiary-500 before:bg-tertiary-500 whitespace-nowrap">
+							Buy again
+						</a>
 					</li>
 					<li>
-						<a href="/" class="text-tertiary-500 before:bg-tertiary-500 whitespace-nowrap"
-							>Gift cards</a
-						>
+						<a href="/" class="text-tertiary-500 before:bg-tertiary-500 whitespace-nowrap">
+							Gift cards
+						</a>
 					</li>
-					<li><a href="/" class="text-tertiary-500 before:bg-tertiary-500">Contact</a></li>
+					<li><a href="/" class="text-tertiary-500 before:bg-tertiary-500"> Contact </a></li>
 				</ul>
 			</nav>
 			<ul slot="actions" class="flex gap-4 text-2xl">
 				<li>
-					<a href="/" class="text-tertiary-500 hover:text-instagram"
-						><i class="fab fa-instagram" /></a
-					>
+					<a href="/" class="text-tertiary-500 hover:text-instagram">
+						<i class="fab fa-instagram" />
+					</a>
 				</li>
 				<li>
-					<a href="/" class="text-tertiary-500 hover:text-facebook"
-						><i class="fab fa-facebook-square" /></a
-					>
+					<a href="/" class="text-tertiary-500 hover:text-facebook">
+						<i class="fab fa-facebook-square" />
+					</a>
 				</li>
 				<li>
-					<a href="/" class="text-tertiary-500 hover:text-youtube"><i class="fab fa-youtube" /></a>
+					<a href="/" class="text-tertiary-500 hover:text-youtube">
+						<i class="fab fa-youtube" />
+					</a>
 				</li>
 			</ul>
 		</Header>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import ProductCard from '$lib/components/ProductCard.svelte';
+	import ProductCard from '$lib/components/product-card/ProductCard.svelte';
 </script>
 
 <div class="max-w-site w-full h-full mx-auto">
