9dbfdc49021ac15d25030e9b8249612582ee7ca4
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-12 05:09:06 +0200

    + product page

--- src/routes/product/+page.svelte
+++ src/routes/product/+page.svelte
@@ -6,6 +6,7 @@
 	<div class="flex-1">
 		<ProductImage
 			images={[
+				'/images/chair.jpg',
 				'/images/gift-1.jpg',
 				'/images/gift-3.jpg',
 				'/images/gift-2.jpg',
@@ -15,5 +16,18 @@
 			]}
 		/>
 	</div>
-	<div class="flex-1" />
+	<div class="flex-1 h-full flex flex-col gap-8">
+		<div>
+			<h2 class="text-6xl">Elegant chair</h2>
+			<div class="text-4xl text-tertiary-600 mt-4">$199</div>
+			<p class="text-xl mt-8 text-tertiary-600">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laudantium qui
+				voluptates! Natus maiores eos sunt, cumque harum deleniti quae recusandae accusantium error
+				praesentium corporis nobis, suscipit expedita itaque accusamus!
+			</p>
+		</div>
+		<div>
+			<button class="btn variant-filled-primary">Add to cart</button>
+		</div>
+	</div>
 </div>
