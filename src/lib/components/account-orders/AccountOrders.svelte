10fa1dc3f80150e3e3d544695d8d6ac2badd0395
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-19 06:24:40 +0200

    + account orders

--- src/lib/components/account-orders/AccountOrders.svelte
+++ src/lib/components/account-orders/AccountOrders.svelte
@@ -1,4 +1,37 @@
 <script lang="ts">
+	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+	import type { TableSource } from '@skeletonlabs/skeleton';
+	const orders = [
+		{
+			orderNumber: '#171132',
+			date: '15 March 2023',
+			status: 'Completed',
+			total: '$299',
+			url: '<a href="/account/order" class="">View <i class="fas fa-external-link-alt"></i></a>'
+		},
+		{
+			orderNumber: '#221134',
+			date: '25 March 2000',
+			status: 'Pending',
+			total: '$1',
+			url: '<a href="/account/order" class="">View <i class="fas fa-external-link-alt"></i></a>'
+		},
+		{
+			orderNumber: '#231321',
+			date: '24 December 2023',
+			status: 'Completed',
+			total: '$420',
+			url: '<a href="/account/order" class="">View <i class="fas fa-external-link-alt"></i></a>'
+		}
+	];
+	const ordersTable: TableSource = {
+		head: ['Order', 'Date', 'Status', 'Total', 'View'],
+		body: tableMapperValues(orders, ['orderNumber', 'date', 'status', 'total', 'url']),
+		meta: tableMapperValues(orders, ['orderNumber', 'date', 'status', 'total', 'url'])
+	};
 </script>
 
 <div class="text-2xl md:text-6xl font-bold mb-8">Orders</div>
+<div>
+	<Table source={ordersTable} element="table shadow-xl" />
+</div>
