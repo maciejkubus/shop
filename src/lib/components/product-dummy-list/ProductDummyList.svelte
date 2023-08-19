93646b46402b01ee53da0f5d3e67fa6f6fc11c14
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-19 05:15:44 +0200

    + lenght prop on product dummy list

--- src/lib/components/product-dummy-list/ProductDummyList.svelte
+++ src/lib/components/product-dummy-list/ProductDummyList.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import ProductTile from '$lib/components/product-tile/ProductTile.svelte';
 	import type { IProduct } from '$lib/interfaces/product.interface';
+	import { onMount } from 'svelte';
 
 	const product: IProduct = {
 		name: 'Elegant Chair',
@@ -8,10 +9,18 @@
 		url: '/product/',
 		image: '/images/chair.jpg'
 	};
+
+	export let lenght = 20;
+
+	let items: number[] = [];
+
+	onMount(() => {
+		items = Array.from({ length: lenght }, (_, i) => i + 1);
+	});
 </script>
 
 <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8">
-	{#each [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20] as item}
+	{#each items as item}
 		<ProductTile {product} class="text-tertiary-500 no-anim" />
 	{/each}
 </div>
