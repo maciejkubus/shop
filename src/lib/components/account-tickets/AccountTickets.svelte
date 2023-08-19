68b1c307930a009eddace48675ccf29d9f6a8c65
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-19 06:45:13 +0200

    + account tickets

--- src/lib/components/account-tickets/AccountTickets.svelte
+++ src/lib/components/account-tickets/AccountTickets.svelte
@@ -1,4 +1,22 @@
 <script lang="ts">
+	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+	import type { TableSource } from '@skeletonlabs/skeleton';
+	const tickets = [
+		{
+			id: '#171132',
+			date: '15 March 2023',
+			status: 'Completed',
+			url: '<a href="/account/ticket" class="">View <i class="fas fa-external-link-alt"></i></a>'
+		}
+	];
+	const ticketsTable: TableSource = {
+		head: ['Id', 'Date', 'Status', 'View'],
+		body: tableMapperValues(tickets, ['id', 'date', 'status', 'url']),
+		meta: tableMapperValues(tickets, ['id', 'date', 'status', 'url'])
+	};
 </script>
 
 <div class="text-2xl md:text-6xl font-bold mb-8">Tickets</div>
+<div>
+	<Table source={ticketsTable} element="table shadow-xl" />
+</div>
