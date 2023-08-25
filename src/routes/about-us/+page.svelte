56f26c15ea583369476af58b0bf5dbdbfc0600b2
Maciej Kubus <maciek.krol.zycia@gmail.com>
2023-08-25 20:06:09 +0200

    + about us page

---
+++ src/routes/about-us/+page.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col-reverse xl:flex-row gap-16">
+	<div class="w-full xl:w-1/2 flex flex-col gap-8 h-full xl:justify-center items-start">
+		<h2 class="text-6xl drop-shadow-xl font-heading-token">Who we are?</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nemo assumenda unde
+			inventore voluptatem, modi asperiores, debitis hic qui esse, ratione labore ad perspiciatis
+			sapiente! Ut voluptate molestias accusamus animi?
+		</p>
+		<a href="/contact/" class="btn variant-filled-primary font-bold text-lg"> Contact us </a>
+	</div>
+	<div class="w-full xl:w-1/2">
+		<img
+			src="/images/office.jpg"
+			alt="office"
+			class="aspect-[3/1] xl:aspect-[4/3] object-cover object-center rounded w-full"
+		/>
+	</div>
+</div>
+<div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-8 py-8">
+	<div class="flex flex-col justify-center items-center text-center py-8 md:p-8">
+		<div class="text-9xl">
+			<i class="fas fa-globe" />
+		</div>
+		<div class="text-3xl font-heading-token pt-6 pb-1">Worldwide</div>
+		<div class="text-tertiary-600">Get free shipping over $65.</div>
+	</div>
+	<div class="flex flex-col justify-center items-center text-center py-8 md:p-8">
+		<div class="text-9xl">
+			<i class="fas fa-box-open" />
+		</div>
+		<div class="text-3xl font-heading-token pt-6 pb-1">Returns</div>
+		<div class="text-tertiary-600">Within 30 days for an exchange.</div>
+	</div>
+	<div class="flex flex-col justify-center items-center text-center py-8 md:p-8">
+		<div class="text-9xl">
+			<i class="fas fa-life-ring" />
+		</div>
+		<div class="text-3xl font-heading-token pt-6 pb-1">Support</div>
+		<div class="text-tertiary-600">Top notch customer service.</div>
+	</div>
+	<div class="flex flex-col justify-center items-center text-center py-8 md:p-8">
+		<div class="text-9xl">
+			<i class="fas fa-credit-card" />
+		</div>
+		<div class="text-3xl font-heading-token pt-6 pb-1">Payments</div>
+		<div class="text-tertiary-600">Flexible payment made easy.</div>
+	</div>
+</div>
+<div class="flex flex-col xl:flex-row gap-16">
+	<div class="w-full xl:w-1/2">
+		<img
+			src="/images/phone.jpg"
+			alt="phone"
+			class="aspect-[3/1] xl:aspect-[4/3] object-cover object-center rounded w-full"
+		/>
+	</div>
+	<div class="w-full xl:w-1/2 flex flex-col gap-8 h-full xl:justify-center items-start">
+		<h2 class="text-6xl drop-shadow-xl font-heading-token">What we do?</h2>
+		<p>
+			Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis nemo assumenda unde
+			inventore voluptatem, modi asperiores, debitis hic qui esse, ratione labore ad perspiciatis
+			sapiente! Ut voluptate molestias accusamus animi?
+		</p>
+		<a href="/contact/" class="btn variant-filled-primary font-bold text-lg"> Contact us </a>
+	</div>
+</div>
